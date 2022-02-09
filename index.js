6a9c6f893234973b0f4ecbf3e65c72dd01b1806b
GuillaumeLariviere <guillaume.lariviere59@gmail.com>
2022-02-09 10:20:16 +0100

    add route contact

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("my first node app")
+const express = require("express");
+const router = express.Router();
+
+const app = express();
+
+const cors = require("cors");
+const corsOption ={
+    origin :["http://localhost:3000"]
+};
+app.use(cors(corsOption));
+
+app.use(router);
+
+router.route("/")
+.get((req, res) =>{
+    console.log("GET /");
+    res.send((req.method + req.path));
+})
+.post((req ,res)=>{
+    res.send((req.method + req.path));
+});
+
+router.route("/test").get((req, res) => {
+    res.send((req.method + req.path));
+});
+
+router.route("/contactscreen").get((req, res) => {
+    console.log((req.method + req.path));
+    res.send("ok");
+    
+});
+const PORT =5000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}.`);
+});
